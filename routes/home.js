64f749957e051b1fee27e8e4d19a0f48c6a62c51
Eric Mignot <eric.mignot@gmail.com>
2024-06-14 09:07:44 -0700

    web folder for frontend

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = (app) => {
     app.get('/', function (_, response) {
         response.setHeader('Content-Type', 'text/html');
-        const html = fs.readFileSync('public/index.html').toString();
+        const html = fs.readFileSync('web/index.html').toString();
 
         response.write(html);
         response.end();
