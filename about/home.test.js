15dabc3f14e06d892fffea6370604e0254672684
Eric Mignot <eric.mignot@gmail.com>
2024-06-14 09:06:57 -0700

    fetch petitions to remove magic number

--- about/home.test.js
+++ about/home.test.js
@@ -2,16 +2,24 @@ const http = require('http');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const config = {
+    beforeParse: (window) => {
+        window.fetch = (url, options) => {
+            return fetch(`http://localhost:5001${url}`, options);
+        };
+    },
     runScripts: 'dangerously',
     resources: 'usable'
 };
 const app = require('../app.js');
 const eventually = require('./eventually.js');
+const seedDbWithPetitionCount = require('./seeding.js');
 
 describe('home page', () => {
     let server;
     let document;
+
     beforeEach(async () => {
+        seedDbWithPetitionCount(2);
         await new Promise((resolve) => {
             server = http.createServer(app).listen(5001, resolve);
         });
