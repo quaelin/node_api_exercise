a0dc89137156019b10fc84000d4b97a52ad3939c
Eric Mignot <eric.mignot@gmail.com>
2024-06-14 09:07:44 -0700

    web folder for frontend

--- routes/webapp.js
+++ routes/webapp.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = (app) => {
     app.get('/webapp.js', function (_, response) {
         response.setHeader('Content-Type', 'application/javascript');
-        const code = fs.readFileSync('public/app.js').toString();
+        const code = fs.readFileSync('web/app.js').toString();
 
         response.write(code);
         response.end();
