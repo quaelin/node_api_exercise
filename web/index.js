f890077b72df36daec6003a132a658c122ea7d11
Eric Mignot <eric.mignot@gmail.com>
2024-06-15 07:53:12 -0700

    inject React app when dom is ready

--- web/index.js
+++ web/index.js
@@ -2,5 +2,4 @@ var ReactDOM = require('react-dom/client');
 var Root = require('./Root');
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<Root />);
+const root = ReactDOM.createRoot(container).render(<Root />);
