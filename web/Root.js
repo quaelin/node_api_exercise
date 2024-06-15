2c2f535ceea6e5b9e7bea588557dc88e08d9d450
Eric Mignot <eric.mignot@gmail.com>
2024-06-15 07:46:40 -0700

    green with React

---
+++ web/Root.js
@@ -0,0 +1,12 @@
+const PetitionCount = require('./PetitionCount');
+
+function Root() {
+    return (
+        <section>
+            <h1>Welcome</h1>
+            <PetitionCount />
+        </section>
+    );
+}
+
+module.exports = Root;
