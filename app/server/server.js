6a82db82157950e811fcc7544363d32fe7d1b040
Eric Mignot <eric.mignot@gmail.com>
2024-07-16 16:52:36 -0700

    start fixed

--- app/server/server.js
+++ app/server/server.js
@@ -4,8 +4,8 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import { app } from './app.js';
-import { getConnection } from './lib/sql.js';
+import { app } from './inbound/app.js';
+import { getConnection } from './db/sql.js';
 const port = 3000;
 
 app.listen(port, async () => {
