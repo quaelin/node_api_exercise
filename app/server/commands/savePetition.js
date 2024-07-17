7819fceec37ff2b92e678bde9dd5e842e8f3cb77
Eric Mignot <eric.mignot@gmail.com>
2024-07-17 14:24:44 -0700

    command uses prepared statement

--- app/server/commands/savePetition.js
+++ app/server/commands/savePetition.js
@@ -1,17 +1,17 @@
-import { runQuery } from '../db/sql';
 import { app } from '../inbound/app';
 
 export const savePetition = (petition) => {
     const sql = `
         insert into petitions (starter_urn, title, body, created_at, updated_at)
-        values
-        (
-            '${petition.starter}',
-            '${petition.title}',
-            '${petition.body}',
-            '${petition.created_at}',
-            '${petition.created_at}'
-        )
-        `;
-    runQuery(app.db, sql);
+        values (:starter_urn, :title, :body, :created_at, :updated_at)
+    `;
+    const stmt = app.db.prepare(sql);
+    stmt.run([
+        petition.starter,
+        petition.title,
+        petition.body,
+        petition.created_at,
+        petition.updated_at
+    ]);
+    stmt.free();
 };
