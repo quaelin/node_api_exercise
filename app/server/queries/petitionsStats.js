c8886ea2e349cf3d8fda0ce5c0bdf681c478471b
Eric Mignot <eric.mignot@gmail.com>
2024-07-18 15:45:59 -0700

    import fixed

--- app/server/queries/petitionsStats.js
+++ app/server/queries/petitionsStats.js
@@ -1,4 +1,4 @@
-import { getRows } from '../db/sql';
+import { getRows } from '../db/sql.js';
 
 export async function getPetitionsStats(app) {
     const stmt = app.db.prepare('SELECT count(*) count FROM petitions;');
